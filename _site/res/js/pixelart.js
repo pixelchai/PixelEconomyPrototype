8bc4fc9fbf9b24f61c04b9ab1c441c5f1f43a1e7
pixelzery <pixelzery@gmail.com>
2021-03-06 18:22:16 +0000

    pixelart.js work

--- _site/res/js/pixelart.js
+++ _site/res/js/pixelart.js
@@ -1,15 +1,51 @@
+const COL_BAKGROUND = "#FFFFFF";
+const COL_GRID = "#000000";
+const DIM = 16; // 16 x 16 pixel art
+
 let c = document.querySelectorAll(".pixelart-canvas")[0];
+let ctx = c.getContext("2d");
 let cWidth = 300;
 let cHeight = 300;
 
 let paletteSelectElement = document.querySelectorAll(".palette-select select")[0];
 let paletteElement = document.querySelectorAll(".palette-select .palette")[0];
 
-function draw() {
+let mouseX = -1;
+let mouseY = -1;
 
+function draw() {
+    // background
+    ctx.fillStyle = COL_BAKGROUND;
+    ctx.rect(0, 0, cWidth, cHeight);
+    ctx.fill();
+
+    // draw grid
+    ctx.strokeStyle = COL_GRID;
+    ctx.setLineDash([1, 1]);
+    let division = cWidth / DIM;
+    for (let i = 0; i <= DIM; i++) {
+        // horizontal
+        ctx.beginPath();
+        ctx.moveTo(0, i * division);
+        ctx.lineTo(cWidth, i * division);
+        ctx.stroke();
+
+        // vertical
+        ctx.beginPath();
+        ctx.moveTo(i * division, 0);
+        ctx.lineTo(i * division, cHeight);
+        ctx.stroke();
+    }
 }
 
 function start() {
+    c.addEventListener('mousemove', function (evt) {
+        let division = cWidth / DIM;
+        var rect = c.getBoundingClientRect();
+
+        mouseX = Math.floor((evt.clientX - rect.left) / division);
+        mouseY = Math.floor((evt.clientY - rect.top) / division);
+    });
     updateUI();
 }
 
