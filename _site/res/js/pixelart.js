87767a9b8442063e3320683214d42e980524ae82
pixelzery <pixelzery@gmail.com>
2021-03-06 17:48:56 +0000

    work on pixelart.js

--- _site/res/js/pixelart.js
+++ _site/res/js/pixelart.js
@@ -1,22 +1,58 @@
 let c = document.querySelectorAll(".pixelart-canvas")[0];
+let cWidth = 300;
+let cHeight = 300;
+
+let paletteSelectElement = document.querySelectorAll(".palette-select select")[0];
+let paletteElement = document.querySelectorAll(".palette-select .palette")[0];
+
 function draw() {
 
 }
 
 function start() {
+    updateUI();
+}
 
+function updateUI() {
+    // sizing
+    c.height = c.offsetWidth;
+    c.style.height = c.offsetWidth + "px";
+
+    cWidth = c.offsetWidth;
+    cHeight = c.offsetHeight;
+
+    // palette dropdown
+    loadPalettes();
+
+    // redraw
+    draw();
+}
+
+function loadPalettes() {
+    let palettes = pixelArtConfig["palettes"];
+    let paletteKeys = Object.keys(palettes);
+
+    for (let i = 0; i < paletteKeys.length; i++) {
+        let paletteKey = paletteKeys[i];
+
+        let optionElement = document.createElement("option");
+        optionElement.innerText = paletteKey;
+
+        paletteSelectElement.appendChild(optionElement);
+    }
+
+    paletteElement.style.width = cWidth - paletteSelectElement.offsetWidth - 10 + "px";
+}
+
+function loadPalette(paletteKey) {
+    let colours = pixelArtConfig["palettes"][paletteKey];
+
+    for (let i = 0; i < colours.length; i++) {
+
+    }
 }
 
 (function () {
-    start();
-
-    function resizeCanvas() {
-        // c.width = c.offsetWidth;
-        c.height = c.offsetWidth;
-        c.style.height = c.offsetWidth + "px";
-
-        draw();
-    }
-    window.addEventListener('resize', resizeCanvas, false);
-    resizeCanvas();
+    window.addEventListener('resize', updateUI);
+    document.addEventListener("DOMContentLoaded", start);
 })();
